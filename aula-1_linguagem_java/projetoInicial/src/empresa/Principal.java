9173f5a750167cc08365362a65abce5572f72625
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-09 16:18:32 -0300

    exemplo com entrada de dados

--- aula-1_linguagem_java/projetoInicial/src/empresa/Principal.java
+++ aula-1_linguagem_java/projetoInicial/src/empresa/Principal.java
@@ -1,5 +1,7 @@
 package empresa; //toda instrução Java termina com ponto e virgula
 
+import java.util.Scanner;
+
 //public = quais classes consegue ver esse arquivo
 public class Principal {
 	//as instruções começam e terminam entre chaves
@@ -8,13 +10,28 @@ public class Principal {
 		//void = o que a função retorna
 		//esta é a função principal que começará a execução do código
 		
-		int idade = 10;
+		//int idade = 10; //variável do tipo de número inteiro
+		//double peso = 72.5; //variável do tipo ponto flutuante
 		
-		idade = idade + 2;
 		System.out.print("Alô Mamãe!"); //exibe a mensagem, mas não quebra linha
 		System.out.println("Alô Papai!");
 		
-		System.out.println("Idade = " + idade);
+		int idade;
+		double peso;
+		
+		//idade = idade + 2;
+		
+		//para conseguir entrar dados
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.print("Digite sua idade: ");
+		idade = teclado.nextInt(); //entrada de valor
+		
+		System.out.print("Digite seu peso: ");
+		peso = teclado.nextDouble();
+		
+		System.out.printf("Idade = %d\n", idade); //exibe mensagem com formatação
+		System.out.printf("Peso = %.2f", peso);
 	}
 
 }
