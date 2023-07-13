94f0c759eb4488932a19ad01b2967340f1fd46d4
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-13 17:07:36 -0300

    adição da classe Carro com seus atributos

--- aula-2_classes_e_objetos/projeto/src/projeto/Principal.java
+++ aula-2_classes_e_objetos/projeto/src/projeto/Principal.java
@@ -3,11 +3,17 @@ package projeto;
 public class Principal {
 	public static void main(String[] args) {
 		Aluno aluno = new Aluno();
-		aluno.matricula= 111;
+		aluno.matricula= 1001;
 		aluno.nome = "Super Mario";
 		aluno.cpf = "111222333";
 		
+		Aluno aluno2 = new Aluno();
+		aluno2.matricula= 1002;
+		aluno2.nome = "Yoshi";
+		aluno2.cpf = "777222555";
+		
 		aluno.info();
+		aluno2.info();
 		
 		aluno.nome = "Luigi";
 		aluno.info();
