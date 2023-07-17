88a41dcc2a22cab5652f351e88376ffdeff256e7
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-17 19:31:51 -0300

    método para settar o atributo hora

--- aula-3_visibilidade/Horario/src/empresa/Horario.java
+++ aula-3_visibilidade/Horario/src/empresa/Horario.java
@@ -4,4 +4,16 @@ public class Horario {
 	private int hora;
 	private int minuto;
 	private int segundo;
+	
+	public int getHora() {
+		return hora;
+	}
+	
+	public void setHora(int hora) {
+		if (hora >= 0 && hora <= 23) {
+			this.hora = hora;
+		} else {
+			System.out.println("Horário inválido!");
+		}
+	}
 }
--- aula-3_visibilidade/Horario/src/empresa/Principal.java
+++ aula-3_visibilidade/Horario/src/empresa/Principal.java
@@ -3,7 +3,7 @@ package empresa;
 public class Principal {
 	public static void main(String[] args) {
 		Horario aula = new Horario();
-		aula.hora = 40;
+		aula.setHora(40);
 		aula.minuto = 30;
 		aula.segundo = 0;
 	}
