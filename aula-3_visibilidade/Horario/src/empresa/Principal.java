299daca47f1e03e540f24183e0c02b7641eef19b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-17 19:24:06 -0300

    adicionando valores aos atributos de Horario

--- aula-3_visibilidade/Horario/src/empresa/Principal.java
+++ aula-3_visibilidade/Horario/src/empresa/Principal.java
@@ -1,5 +1,10 @@
 package empresa;
 
 public class Principal {
-
+	public static void main(String[] args) {
+		Horario aula = new Horario();
+		aula.hora = 40;
+		aula.minuto = 30;
+		aula.segundo = 0;
+	}
 }
