5fc275d88d2e118cc7d91d04968941248136a448
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-18 19:02:27 -0300

    explorando LinkedList

--- aula-3_visibilidade/ProjetoCollection/src/colecoes/Principal.java
+++ aula-3_visibilidade/ProjetoCollection/src/colecoes/Principal.java
@@ -1,15 +1,23 @@
 package colecoes;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Principal {
 	public static void main(String[] args) {
-		ArrayList<String> pessoas = new ArrayList();
+		//ArrayList<String> pessoas = new ArrayList();
+		LinkedList<String> pessoas = new LinkedList();
 		
 		pessoas.add("Mario");
 		pessoas.add("Luigi");
 		pessoas.add("Peach");
 		pessoas.add("Yoshi");
+		
+		System.out.println(pessoas);
+		
+		System.out.println(pessoas.get(0));
+		
+		pessoas.remove(2);
 		
 		System.out.println(pessoas);
 	}
