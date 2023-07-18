48d5044bb5fca7e3339b6d1bf542c375bfd65932
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-18 19:11:44 -0300

    exemplo de HashMap

--- aula-3_visibilidade/ProjetoCollection/src/colecoes/Principal.java
+++ aula-3_visibilidade/ProjetoCollection/src/colecoes/Principal.java
@@ -1,12 +1,16 @@
 package colecoes;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Principal {
 	public static void main(String[] args) {
 		//ArrayList<String> pessoas = new ArrayList();
 		LinkedList<String> pessoas = new LinkedList();
+		
+		//como ambos heram de listas
+		//possuem muitas semelhanças
 		
 		pessoas.add("Mario");
 		pessoas.add("Luigi");
@@ -20,5 +24,14 @@ public class Principal {
 		pessoas.remove(2);
 		
 		System.out.println(pessoas);
+		
+		HashMap<String, String> capitais = new HashMap();
+		capitais.put("Brasil", "Brasília");
+		capitais.put("Argentina", "Buenos Aires");
+		capitais.put("Paraguai", "Assunção");
+		capitais.put("Uruguai", "Montevidéu");
+		
+		System.out.println(capitais);
+		System.out.println(capitais.get("Brasil"));
 	}
 }
