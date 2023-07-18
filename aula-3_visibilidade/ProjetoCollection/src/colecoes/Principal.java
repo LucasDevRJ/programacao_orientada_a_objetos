27355d1874f5205f4e8f5a62ee96b3bd735a16a7
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-18 20:07:57 -0300

    exemplo de código com Iterator

--- aula-3_visibilidade/ProjetoCollection/src/colecoes/Principal.java
+++ aula-3_visibilidade/ProjetoCollection/src/colecoes/Principal.java
@@ -3,6 +3,8 @@ package colecoes;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Principal {
@@ -38,6 +40,14 @@ public class Principal {
 		
 		System.out.println(pessoas);
 		
+		//reverte a lista
+		Collections.reverse(pessoas);
+		
+		System.out.println(pessoas);
+		
+		System.out.println(Collections.min(pessoas));
+		System.out.println(Collections.max(pessoas));
+		
 		HashMap<String, String> capitais = new HashMap();
 		capitais.put("Brasil", "Brasília");
 		capitais.put("Argentina", "Buenos Aires");
@@ -46,5 +56,29 @@ public class Principal {
 		
 		System.out.println(capitais);
 		System.out.println(capitais.get("Brasil"));
+		
+		ArrayList<Integer> lista = new ArrayList();
+		HashSet<Integer> conjunto = new HashSet();
+		HashMap<String, Integer> mapa = new HashMap();
+		
+		int soma;
+		soma = 0;
+		for (int i = 0; i < lista.size(); i++) {
+			soma += lista.get(i);
+		}
+		
+		for (int item : lista) {
+			soma += item;
+		}
+		
+		soma = 0;
+		//Iterator it = mapa.entrySet().iterator();
+		//Iterator it = conjunto.iterator();
+		Iterator it = lista.iterator();
+		
+		while (it.hasNext()) {
+			soma += (int) it.next();
+		}
+		
 	}
 }
