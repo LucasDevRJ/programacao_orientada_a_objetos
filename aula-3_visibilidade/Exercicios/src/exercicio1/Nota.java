3d7376cae955f56fc0dd4c230fd87b9ce77c9d60
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-19 16:25:16 -0300

    encapsulando nota1 e nota2

--- aula-3_visibilidade/Exercicios/src/exercicio1/Nota.java
+++ aula-3_visibilidade/Exercicios/src/exercicio1/Nota.java
@@ -1,5 +1,22 @@
 package exercicio1;
 
 public class Nota {
-
+	private double nota1;
+	private double nota2;
+	
+	public double getNota1() {
+		return nota1;
+	}
+	
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	
+	public double getNota2() {
+		return nota2;
+	}
+	
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
 }
