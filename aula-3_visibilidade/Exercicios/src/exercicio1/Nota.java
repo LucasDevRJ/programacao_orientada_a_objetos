bdf5f056d6c5a2ed92a7f6bc689507651a12caa3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-19 16:39:16 -0300

    método para tirar a média de notas

--- aula-3_visibilidade/Exercicios/src/exercicio1/Nota.java
+++ aula-3_visibilidade/Exercicios/src/exercicio1/Nota.java
@@ -19,4 +19,16 @@ public class Nota {
 	public void setNota2(double nota2) {
 		this.nota2 = nota2;
 	}
+	
+	public void exibeResultadoMedia() {
+		double media = (nota1 + nota2) / 2;
+		if (media >= 6.0) {
+			System.out.println("O aluno está aprovado!");
+		} else if (media >= 4.0) {
+			System.out.println("O aluno está de prova final");
+			System.out.println("O aluno precisará tirar no minímo 6.0");
+		} else {
+			System.out.println("O aluno está reprovado!");
+		}
+	}
 }
