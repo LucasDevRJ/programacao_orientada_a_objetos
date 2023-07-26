8d5160e451cfbac8f2766fd48f518ea82acf2a74
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-26 19:06:30 -0300

    adição da classe LivroDigital

---
+++ aula-4_heranca/Exercicio/src/exercicio/LivroDigital.java
@@ -0,0 +1,22 @@
+package exercicio;
+
+public class LivroDigital {
+	private int download;
+	private int tamanho;
+	
+	public int getDownload() {
+		return download;
+	}
+	
+	public void setDownload(int download) {
+		this.download = download;
+	}
+	
+	public int getTamanho() {
+		return tamanho;
+	}
+	
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+}
