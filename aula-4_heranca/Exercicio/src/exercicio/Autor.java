9b22175879c5780a87e23da9afde0fccc0cc0e4d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-26 18:51:22 -0300

    criação da classe Autor

---
+++ aula-4_heranca/Exercicio/src/exercicio/Autor.java
@@ -0,0 +1,31 @@
+package exercicio;
+
+public class Autor {
+	private String nome;
+	private String email;
+	private String nacionalidade;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+	
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+}
