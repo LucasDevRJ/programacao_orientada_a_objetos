c9ee881f5d5de3e2e762cd93ba9724359adecff7
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-26 19:05:06 -0300

    adição da classe LivroDigital

---
+++ aula-4_heranca/Exercicio/src/exercicio/LivroFisico.java
@@ -0,0 +1,22 @@
+package exercicio;
+
+public class LivroFisico {
+	private int tiragem;
+	private int peso;
+	
+	public int getTiragem() {
+		return tiragem;
+	}
+	
+	public void setTiragem(int tiragem) {
+		this.tiragem = tiragem;
+	}
+	
+	public int getPeso() {
+		return peso;
+	}
+	
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+}
