db45db68b5133a23133ea3d37fd036e570062d15
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-26 21:20:40 -0300

    classe Funcionario

---
+++ aula-5_polimorfismo/Polimorfismo/src/polimorfismo/Funcionario.java
@@ -0,0 +1,14 @@
+package polimorfismo;
+
+public class Funcionario {
+	String nome;
+	
+	public Funcionario(String nome) {
+		this.nome = nome;
+	}
+	
+	public float pagamento() {
+		System.out.println("Processando pagamento");
+		return 0.0f;
+	}
+}
