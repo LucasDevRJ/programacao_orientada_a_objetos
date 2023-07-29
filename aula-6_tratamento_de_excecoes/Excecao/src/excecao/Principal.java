5d6a6c3b39d0a7cfe5431943dd44c2aeedebaba2
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-29 20:34:20 -0300

    multiplo try catch e finally

--- aula-6_tratamento_de_excecoes/Excecao/src/excecao/Principal.java
+++ aula-6_tratamento_de_excecoes/Excecao/src/excecao/Principal.java
@@ -6,9 +6,15 @@ public class Principal {
 		
 		
 		try {
+			numeros[0] = numeros[0] / 0; //cairá na Exception
 			System.out.println(numeros[10]);
-		} catch(Exception e) {
+		} catch(ArrayIndexOutOfBoundsException e) { //exceção especifica
+			System.out.println("Fora do limite: " + e.getMessage());
+			return;
+		} catch(Exception e) { //exceção generica
 			System.out.println("Ocorreu um problema: " + e.getMessage());
+		} finally { //será executado de todo o modo
+			System.out.println("Finalizando o programa.");
 		}
 	}
 }
