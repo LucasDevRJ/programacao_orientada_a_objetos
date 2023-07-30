87c03032e1848d06de3f924dfc27ec6e1a7bd19f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-29 21:21:08 -0300

    comparando com equals

--- aula-6_tratamento_de_excecoes/Excecao/src/excecao/Principal.java
+++ aula-6_tratamento_de_excecoes/Excecao/src/excecao/Principal.java
@@ -22,5 +22,22 @@ public class Principal {
 		//será verdadeiro porque comparam o coteúdo
 		System.out.println(string1.equals(string2));
 		System.out.println(string1.equals(string3));
+		
+		System.out.println();
+		
+		Usuario usuario1 = new Usuario(1, "Jorge", "111.111.111-11");
+		Usuario usuario2 = new Usuario(2, "Fábio", "222.222.22-22");
+		Usuario usuario3 = usuario1;
+		
+		System.out.println(usuario1 == usuario2);
+		System.out.println(usuario1 == usuario3);
+		
+		System.out.println();
+		
+		//irá dar falso pq o conteúdo é distinto
+		System.out.println(usuario1.equals(usuario2));
+		
+		//irá dar verdadeiro pq o conteúdo é o mesmo
+		System.out.println(usuario1.equals(usuario3));
 	}
 }
--- aula-6_tratamento_de_excecoes/Excecao/src/excecao/Usuario.java
+++ aula-6_tratamento_de_excecoes/Excecao/src/excecao/Usuario.java
@@ -1,5 +1,7 @@
 package excecao;
 
+import java.util.Objects;
+
 public class Usuario {
 	int id;
 	String nome;
@@ -9,5 +11,22 @@ public class Usuario {
 		this.id = id;
 		this.nome = nome;
 		this.cpf = cpf;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, id, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(cpf, other.cpf) && id == other.id && Objects.equals(nome, other.nome);
 	}
 }
